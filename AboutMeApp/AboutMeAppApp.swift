18a39576c130bace9b59c1203fa6c39a51b82c47
Ananya <ananyatheavenger@gmail.com>
2025-07-23 14:55:38 -0500

    Initial Commit

---
+++ AboutMeApp/AboutMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeAppApp.swift
+//  AboutMeApp
+//
+//  Created by Ananya Gogula on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
